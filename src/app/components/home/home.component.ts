390f99a15f203975f82e511376290dfb7e1e3b2e
Jesus <Ing.estevezaquino@gmail.com>
2020-03-15 00:10:25 -0400

    Se diseno el apartado de Nosotros(alpha) y se prepararon las bases para trabjar con servicios, todos pertenecientes al modulo Home.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,10 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   navItem:any;
   contenedorP:any;
+  contenedorN:any;
+  contenedorS:any;
   AlturaNav:any;
   AlturaCon:any;
 
@@ -18,10 +21,41 @@ export class HomeComponent implements OnInit {
     this.AlturaCon = screen.height;
     this.navItem = document.getElementById("barra-navegacion");
     this.AlturaNav = this.navItem.clientHeight;
-    this.contenedorP = document.getElementById("wallpaper-home");
+    this.contenedorP = document.getElementById("home");
+    this.contenedorN = document.getElementById("nosotros");
+    this.contenedorS = document.getElementById("servicios");
   }
 
   ngAfterViewInit(){
-    this.contenedorP.style.height = ""+(this.AlturaCon - this.AlturaNav*2)+"px";
+    this.contenedorP.style.height = ""+(this.AlturaCon - this.AlturaNav)+"px";
+    this.contenedorN.style.height = ""+(this.AlturaCon - this.AlturaNav)+"px";
+    this.contenedorS.style.height = ""+(this.AlturaCon - this.AlturaNav*2)+"px";
+  }
+
+  navigateToHome(){
+    var element = document.getElementById("home");
+    element.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth', 
+      inline: 'start'
+    });
+  }
+
+  navigateToNosotros(){
+    var element = document.getElementById("nosotros");
+    element.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth', 
+      inline: 'start'
+    });
+  }
+
+  navigateToServicios(){
+    var element = document.getElementById("servicios");
+    element.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth', 
+      inline: 'start'
+    });
   }
 }
